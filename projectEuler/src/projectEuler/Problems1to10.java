ca887438e1baacbbeb6cb080981200c582202ab8
haredega <hgarcia.ee@gmail.com>
2015-06-26 21:45:24 -0300

    Implements fibonacci with an array

--- projectEuler/src/projectEuler/Problems1to10.java
+++ projectEuler/src/projectEuler/Problems1to10.java
@@ -2,6 +2,8 @@ package projectEuler;
 
 public class Problems1to10 {
 
+	public static long[] vet_prob02 = new long[100];	
+	
 	public static int problem1(int div1, int div2, int max) {
 	// sum of all the multiple of div1 and div2 below the max value.
 			int sum=0;
@@ -14,11 +16,29 @@ public class Problems1to10 {
 			return sum;
 	}
 	
-	public static int fibonacci(int arg){
-		if (arg==0|| arg==1) return 1;
+	//Calculates the n-th number in the fibonacci sequence.
+	public static double fibonacci(double arg){
+		if (arg==1) return 1;
 		else if (arg==2) return 2;
-		else return (fibonacci(arg-1)+fibonacci(arg-2));
+		else return fibonacci(arg-1) + fibonacci(arg-2);
+
+	}
+
+	public static long problem02(long max){
+		int i;
+		vet_prob02[0]=1;
+		vet_prob02[1]=2;
 		
+		
+		for(i=2; vet_prob02[i]< max && i<99 ; i++){
+			vet_prob02[i]=vet_prob02[i-1]+vet_prob02[i-2];			
+		}
+		return vet_prob02[i];
 		
 	}
+	
+	
+
+
 }
+	
--- projectEuler/src/projectEuler/ProjectEuler.java
+++ projectEuler/src/projectEuler/ProjectEuler.java
@@ -5,11 +5,11 @@ public class ProjectEuler {
 	public static void main(String[] args) {
 		
 		//Problem 2 - Finding the sum of the even Fibonacci sequence under 4 millions 
+		System.out.println("The number is:" +Problems1to10.problem02(50) +".\n");
+		//System.out.println("The max fib under 100 is is:" +Problems1to10.maxFibonnaciUnder(100)+".\n");
+		//System.out.println("The 4millionth fibonacci number is:" +Problems1to10.fibonacci(50)+".\n");
+		//double a= Problems1to10.evenFibonacciSum(100);
+		//System.out.println("The sum of the even fibonacci numbers below 4million is:" +a+".\n");
 		
-		
-		
-		System.out.println("The n-th fibonacci number is:" +Problems1to10.fibonacci(4)+".\n");
-		System.out.println("The n-th fibonacci number is:" +Problems1to10.fibonacci(5)+".\n");
-		System.out.println("The n-th fibonacci number is:" +Problems1to10.fibonacci(10)+".\n");
 }
 }
